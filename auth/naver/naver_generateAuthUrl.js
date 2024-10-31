e87490bca425f60ca693890a6775b381bfaac54a
thegicode <thegi.code@gmail.com>
2024-10-31 21:18:36 +0900

    update

--- auth/naver/naver_generateAuthUrl.js
+++ auth/naver/naver_generateAuthUrl.js
@@ -28,5 +28,6 @@ export default function generateNaverAuthUrl() {
 
     // URLSearchParams로 파라미터를 문자열로 변환하고, 네이버 인증 URL에 추가
     const url = `${naverAuthUrl}?${new URLSearchParams(params)}`;
-    window.location.href = url;
+    console.log(url);
+    // window.location.href = url;
 }
