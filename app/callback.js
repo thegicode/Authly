9da559693863c27b71b73cb8e81f6a921e52c4ee
thegicode <thegi.code@gmail.com>
2024-11-01 17:41:58 +0900

    update

--- app/callback.js
+++ app/callback.js
@@ -10,23 +10,23 @@ function getCodeAndStateFromUrl() {
     };
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    // 3. 네이버에서 인증 코드 받기
-    const { code, state } = getCodeAndStateFromUrl();
-    console.log("Code:", code);
-    console.log("State:", state);
+// document.addEventListener("DOMContentLoaded", async () => {
+// 3. 네이버에서 인증 코드 받기
+const { code, state } = getCodeAndStateFromUrl();
+console.log("Code:", code);
+console.log("State:", state);
 
-    // 4. 인증 코드로 Access Token 발급
-    const accessToken = await getAccessToken(code, state);
-    console.log("accessToken", accessToken);
+// 4. 인증 코드로 Access Token 발급
+const accessToken = await getAccessToken(code, state);
+console.log("accessToken", accessToken);
 
-    // 5. 사용자 정보 요청
-    const userInfo = await getUserInfo(accessToken);
-    console.log("userInfo", userInfo);
+// 5. 사용자 정보 요청
+const userInfo = await getUserInfo(accessToken);
+console.log("userInfo", userInfo);
 
-    // 6. 사용자 정보로 회원 가입/로그인 처리
-    // 네이버에서 가져온 사용자 정보를 바탕으로 세션을 생성하고, 기존 회원이 아니라면 회원 가입 절차를 진행합니다.
-});
+// 6. 사용자 정보로 회원 가입/로그인 처리
+// 네이버에서 가져온 사용자 정보를 바탕으로 세션을 생성하고, 기존 회원이 아니라면 회원 가입 절차를 진행합니다.
+// });
 
 // var naverLogin = new naver.LoginWithNaverId({
 //     clientId: localStorage.getItem("NAVER_CLIENT_ID"), // 로컬 스토리지에서 Client ID 불러오기
