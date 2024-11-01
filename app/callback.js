aba9792af9e0558bc8582157538f2b3f8c3fa4a6
thegicode <thegi.code@gmail.com>
2024-11-01 17:21:24 +0900

    update

--- app/callback.js
+++ app/callback.js
@@ -1,28 +1,45 @@
-import getAccessToken from "../auth/naver/naver_accesToken.js";
-import getUserInfo from "../auth/naver/naver_getUser.js";
+// import getAccessToken from "../auth/naver/naver_accesToken.js";
+// import getUserInfo from "../auth/naver/naver_getUser.js";
 
-// URL에서 code와 state를 추출하는 함수
-function getCodeAndStateFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return {
-        code: urlParams.get("code"),
-        state: urlParams.get("state"),
-    };
-}
+// // URL에서 code와 state를 추출하는 함수
+// function getCodeAndStateFromUrl() {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     return {
+//         code: urlParams.get("code"),
+//         state: urlParams.get("state"),
+//     };
+// }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    // 3. 네이버에서 인증 코드 받기
-    const { code, state } = getCodeAndStateFromUrl();
-    console.log("Code:", code);
-    console.log("State:", state);
+// document.addEventListener("DOMContentLoaded", async () => {
+//     // 3. 네이버에서 인증 코드 받기
+//     const { code, state } = getCodeAndStateFromUrl();
+//     console.log("Code:", code);
+//     console.log("State:", state);
 
-    // 4. 인증 코드로 Access Token 발급
-    const accessToken = await getAccessToken(code, state);
-    console.log("accessToken", accessToken);
+//     // 4. 인증 코드로 Access Token 발급
+//     const accessToken = await getAccessToken(code, state);
+//     console.log("accessToken", accessToken);
 
-    // 5. 사용자 정보 요청
-    const userInfo = await getUserInfo(accessToken);
+//     // 5. 사용자 정보 요청
+//     const userInfo = await getUserInfo(accessToken);
 
-    // 6. 사용자 정보로 회원 가입/로그인 처리
-    // 네이버에서 가져온 사용자 정보를 바탕으로 세션을 생성하고, 기존 회원이 아니라면 회원 가입 절차를 진행합니다.
+//     // 6. 사용자 정보로 회원 가입/로그인 처리
+//     // 네이버에서 가져온 사용자 정보를 바탕으로 세션을 생성하고, 기존 회원이 아니라면 회원 가입 절차를 진행합니다.
+// });
+
+var naverLogin = new naver.LoginWithNaverId({
+    clientId: window.env.NAVER_CLIENT_ID,
+    callbackUrl: window.env.NAVER_REDIRECT_URI,
+});
+
+naverLogin.init();
+
+naverLogin.getLoginStatus((status) => {
+    if (status) {
+        var userId = naverLogin.user.getId();
+        var userName = naverLogin.user.getName();
+        document.body.innerHTML = "<h3>" + userName + "님, 로그인 성공!</h3>";
+    } else {
+        console.log("로그인 실패");
+    }
 });
--- app/index.js
+++ app/index.js
@@ -1,6 +1,28 @@
-import generateNaverAuthUrl from "../auth/naver/naver_generateAuthUrl.js";
+// import generateNaverAuthUrl from "../auth/naver/naver_generateAuthUrl.js";
 
-document.querySelector("#naver-login_button").addEventListener("click", () => {
-    //  2. 네이버 로그인 요청 URL 구성
-    generateNaverAuthUrl();
+// document.querySelector("#naver-login_button").addEventListener("click", () => {
+//     //  2. 네이버 로그인 요청 URL 구성
+//     generateNaverAuthUrl();
+// });
+
+var naverLogin = new naver.LoginWithNaverId({
+    clientId: window.env.NAVER_CLIENT_ID, // 네이버 개발자 센터에서 발급받은 Client ID
+    callbackUrl: window.env.NAVER_REDIRECT_URI, // 설정한 Redirect URI와 동일하게 설정
+    isPopup: false, // 팝업 형태로 로그인을 사용할지 여부
+    loginButton: { color: "green", type: 3, height: 45 }, // 로그인 버튼 스타일 지정
+});
+
+// 네이버 로그인 초기화
+naverLogin.init();
+
+// 로그인 후 처리
+naverLogin.getLoginStatus((status) => {
+    if (status) {
+        // 로그인 성공 시, 사용자 정보 출력
+        var userId = naverLogin.user.getId();
+        var userName = naverLogin.user.getName();
+        alert("로그인 성공: " + userName + "님");
+    } else {
+        console.log("로그인 실패");
+    }
 });
